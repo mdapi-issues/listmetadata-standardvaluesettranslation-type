0cc1e54a397894d8c80f83fb27bead34b0ad0a67
Matthias Rolke <mr.amtrack@gmail.com>
2020-11-26 16:28:20 +0100

    test: don't use fake FileProperties

--- test/workaround.test.ts
+++ test/workaround.test.ts
@@ -1,11 +1,16 @@
 import { expect } from 'chai';
+import { promises } from 'fs';
+import * as path from 'path';
 import { fixNilType } from '../src/workaround';
-import * as actual from './fixtures/actual.json';
 import * as describeMetadataResult from './fixtures/describeMetadataResult.json';
 import * as expected from './fixtures/expected.json';
 
 describe('workaround', function () {
   it('fixes the type in FileProperties for StandardValueSetTranslation', async () => {
+    const buf = await promises.readFile(
+      path.join(__dirname, 'fixtures', 'actual.json')
+    );
+    const actual = JSON.parse(buf.toString());
     const result = fixNilType(actual, describeMetadataResult);
     expect(result).to.deep.equal(expected);
   });
